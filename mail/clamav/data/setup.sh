750c2302af8032936e517c776fe84b969b263be6
real_M4C <sag_i_dir_ned@gmx.at>
2017-07-25 05:48:59 +0200

    Create setup.sh

---
+++ mail/clamav/data/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apk add --no-cache clamav clamav-libunrar clamav-daemon bash curl
+
+# pre-download defintions
+set -ex
+/tmp/data/dl_files.sh
+apk del curl
+
+# permission juggling
+mkdir -p /run/clamav
+chown clamav:clamav /run/clamav
+chmod 750 /run/clamav
+
+# av configuration update
+sed -i 's/^Foreground .*$/Foreground true/g' /etc/clamav/clamd.conf
+echo "TCPSocket 3310" >> /etc/clamav/clamd.conf
+sed -i 's/^Foreground .*$/Foreground true/g' /etc/clamav/freshclam.conf
