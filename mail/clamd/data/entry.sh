1294c3a5bb8f319fdd6b9c2acbdbdd71705bc4dd
real_M4C <sag_i_dir_ned@gmx.at>
2017-07-26 20:04:43 +0200

    Update entry.sh

--- mail/clamd/data/entry.sh
+++ mail/clamd/data/entry.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 trap "kill 0" SIGINT
 
+TZ=${TZ:"Europe/Berlin"}
+cp /usr/share/zoneinfo/${TZ} /etc/localtime
+
 touch /var/log/clamav/clamd.log /var/log/clamav/freshclam.log
 chown -R clamav:clamav /var/log/clamav/
 
