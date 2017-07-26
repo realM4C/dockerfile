9ff037a1696df1e52d4f25c980fbdd320f8efcd4
real_M4C <sag_i_dir_ned@gmx.at>
2017-07-26 16:23:30 +0200

    Create setup.sh

---
+++ mail/unbound/data/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+cp /tmp/entry.sh /entry.sh
+cp /tmp/unbound.conf /etc/unbound.conf
+mkdir /etc/unbound/conf.d
+
+unbound-anchor -a /etc/unbound/root.key
+unbound-control-setup
+curl -s ftp://ftp.internic.net/domain/named.cache > /etc/unbound/root.hints
+
+apk del --no-cache curl
+
+# permission juggling
+chown -R unbound:unbound /etc/unbound/
