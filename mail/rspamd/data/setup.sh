21a6a059e48ba51bee18c103f39152af07bc41e6
real_M4C <sag_i_dir_ned@gmx.at>
2017-07-27 19:15:30 +0200

    Update setup.sh

--- mail/rspamd/data/setup.sh
+++ mail/rspamd/data/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -ex
+# For Alpine Linux:
+#echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+#apk add --no-cache rspamd rspamd-controller tzdata
+
 apt-key adv --fetch-keys http://rspamd.com/apt-stable/gpg.key
 echo "deb http://rspamd.com/apt-stable/ jessie main" > /etc/apt/sources.list.d/rspamd.list
 apt-get update
@@ -10,3 +14,7 @@ rm -rf /var/lib/apt/lists/*
 
 cp /tmp/entry.sh /entry.sh
 cp /usr/share/zoneinfo/${TZ} /etc/localtime
+
+mkdir /run/rspamd
+#chown rspamd:rspamd /run/rspamd
+chown _rspamd:_rspamd /run/rspamd
