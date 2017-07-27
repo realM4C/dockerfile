9c1924b6cb87df39bdedf9f022da31101d405c6b
real_M4C <sag_i_dir_ned@gmx.at>
2017-07-27 17:29:12 +0200

    Update setup.sh

--- mail/rspamd/data/setup.sh
+++ mail/rspamd/data/setup.sh
@@ -2,7 +2,7 @@
 
 set -ex
 echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-apk add --no-cache rspamd tzdata
+apk add --no-cache rspamd rspamd-controller tzdata
 
 cp /tmp/entry.sh /entry.sh
 cp /usr/share/zoneinfo/${TZ} /etc/localtime
