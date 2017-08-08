38fc8835229cb871a1577a739ef167c47d4f805f
real_M4C <sag_i_dir_ned@gmx.at>
2017-08-08 09:19:55 +0200

    Update setup.sh

--- mail/postfix/data/setup.sh
+++ mail/postfix/data/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
 set -ex
 
 apk add --no-cache bash postfix postfix-mysql postfix-pcre rsyslog tzdata
+
+cp /tmp/rsyslog.conf /etc/rsyslog.conf
+cp /tmp/conf /etc/postfix
 
 cp /tmp/entry.sh /entry.sh
 cp /usr/share/zoneinfo/${TZ} /etc/localtime
