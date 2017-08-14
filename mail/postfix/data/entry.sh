7f7a6bbd614baa831e71db7c5cbd0ec65f873bec
real_M4C <sag_i_dir_ned@gmx.at>
2017-08-14 07:05:48 +0200

    Update entry.sh

--- mail/postfix/data/entry.sh
+++ mail/postfix/data/entry.sh
@@ -1,9 +1,9 @@
-
+#!/bin/sh
 
 
 # Actually run Postfix
 rm -f /var/run/rsyslogd.pid
-chown -R postfix: /queue
+chown -R postfix: /var/spool/postfix/
 /usr/lib/postfix/post-install meta_directory=/etc/postfix create-missing
 /usr/lib/postfix/master &
 exec rsyslogd -n
